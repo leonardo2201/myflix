ee876232dfbdfcedf7a86863f7f801f19c8be198
Leonardo Cristiano de Oliveira <leo.cristiano.oliveira@gmail.com>
2020-07-29 12:00:48 -0300

    Update index.js

--- src/components/Menu/index.js
+++ src/components/Menu/index.js
@@ -8,7 +8,7 @@ function Menu() {
   return (
     <nav className="Menu">
       <a href="/">
-        <img className="Logo" src={Logo} alt="AluraFlix logo" />
+        <img className="Logo" src={Logo} alt="MyFlix logo" />
       </a>
 
       <Button as="a" className="ButtonLink" href="/">
